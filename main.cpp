e198c7b01138f809b8d5df66a4d2f661038c7af8
justicsc <82329123+justicsc@users.noreply.github.com>
2021-10-22 17:40:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Check{
+    int CheckNum;
+    string Checkmemo;
+    int CheckAmount;
+
+};
+
+const int N =5;
+
+class CheckBook{
+public:
+
+
+private:
+    
+
+
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
