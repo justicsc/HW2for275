4d6d25c2c373de8c4e1a1347899dab0bf2cf5dc9
justicsc <82329123+justicsc@users.noreply.github.com>
2021-10-22 22:01:45 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ struct Check{
     int CheckNum;
     string Checkmemo;
     int CheckAmount;
+   /* bool operator >(const Check& bal){
+        if(CheckAmount > bal){
+            cout<<"This amount is greater"<<endl;
+            return true;
+        }
+        return false;
+        cout<<"This amount is lesser"<<endl;
+    };
+*/
+   ostream& operator << (const Check& CheckAmount);
 
 };
 
@@ -14,11 +24,28 @@ const int N =5;
 
 class CheckBook{
 public:
+    CheckBook():balance(0), numofChecks(0), checkBookSize(4){}
+    void deposit();
+    Check *chkPtr();
+    int lastDeposit();
+    float getBalance(){return balance;}
+    int getNumOfChecks(){return numofChecks;}
+    int getcheckBookSize(){return checkBookSize;}
+    float setBalance(float bal){balance=bal;}
+    int setNumOfChecks(int numC){numofChecks=numC;}
+    int setcheckBookSize(int bookS){checkBookSize=bookS;}
+    bool writeCheck(float c_amount ){
+        if(c_amount < balance){
 
+         return true;
+        }
+        return false;
 
+    }
 private:
-    
-
+   int numofChecks;
+   int checkBookSize;
+   float balance;
 
 
 };
@@ -27,3 +54,4 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
